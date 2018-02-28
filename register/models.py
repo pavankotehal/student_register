f503a20fc171ccc98b7b6895bde604b4d501491c
Pavan Kotehal <pavan.kotehal@gmail.com>
2018-02-28 19:06:01 +1100

    enabling DRF for API endpoints

--- register/models.py
+++ register/models.py
@@ -2,18 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Student(models.Model):
-    fname = models.CharField(max_length=50)
-    lname = models.CharField(max_length=50)
-    age = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
+ELECTRONICS_COMMUNICATIONS = 'EC'
+COMPUTER_SCIENCE = 'CS'
+INFORMATION_SCIENCE = 'IS'
+MECHANICAL = 'ME'
+INSTRUMENTATION = 'IT'
 
-    ELECTRONICS_COMMUNICATIONS = 'EC'
-    COMPUTER_SCIENCE = 'CS'
-    INFORMATION_SCIENCE = 'IS'
-    MECHANICAL = 'ME'
-    INSTRUMENTATION = 'IT'
-    branches_college = (
+branches_college = (
         (ELECTRONICS_COMMUNICATIONS, 'Electronics & Communication'),
         (COMPUTER_SCIENCE, 'Computer Science'),
         (INFORMATION_SCIENCE, 'Informatin Science'),
@@ -21,22 +16,28 @@ class Student(models.Model):
         (INSTRUMENTATION, 'Instrumentation'),
     )
 
+FRESHMAN = 'FR'
+SOPHOMORE = 'SO'
+JUNIOR = 'JR'
+SENIOR = 'SR'
+
+YEAR_IN_SCHOOL_CHOICES = (
+    (FRESHMAN, 'Freshman'),
+    (SOPHOMORE, 'Sophomore'),
+    (JUNIOR, 'Junior'),
+    (SENIOR, 'Senior'),
+)
+
+
+class Student(models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    age = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
     branches = models.CharField(
         max_length=2,
         choices=branches_college,
         default=ELECTRONICS_COMMUNICATIONS
-    )
-
-    FRESHMAN = 'FR'
-    SOPHOMORE = 'SO'
-    JUNIOR = 'JR'
-    SENIOR = 'SR'
-
-    YEAR_IN_SCHOOL_CHOICES = (
-        (FRESHMAN, 'Freshman'),
-        (SOPHOMORE, 'Sophomore'),
-        (JUNIOR, 'Junior'),
-        (SENIOR, 'Senior'),
     )
     year_in_school = models.CharField(
         max_length=2,
@@ -49,3 +50,6 @@ class Student(models.Model):
 
     def is_upperclass(self):
         return self.year_in_school(self.JUNIOR, self.SENIOR)
+
+    class Meta:
+        ordering = ('created_at',)
--- register/urls.py
+++ register/urls.py
@@ -8,4 +8,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.student_list, name='student_list'),
+    url(r'^students/$', views.list_student),
+    url(r'^students/(?P<pk>[0-9]+)/$', views.student_detail),
 ]
